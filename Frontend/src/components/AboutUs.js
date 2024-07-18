9862f793724ebe47e908bc3658df5ef1491ec323
Ayush <ayushpro111@gmail.com>
2024-07-19 01:49:21 +0530

    nav and aboutus updated

--- Frontend/src/components/AboutUs.js
+++ Frontend/src/components/AboutUs.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const AboutUs = () => {
     return (
-        <div className=' py-6 mb-16 hidden xl:block'>
-            <div className='container flex items-center justify-between '>
+        <div className=' py-6 mb-16 '>
+            <div className='container lg:flex   justify-between '>
                 <img src="https://imgs.search.brave.com/MgyL2QfqkKo0YmmE8yczZ1H7Rla0_LAiqMwFxtnSSxw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM2/OTc1NDIzOS9waG90/by91bml2ZXJzaXR5/LXN0dWRlbnQtaW4t/d2hpdGUtYmFja2dy/b3VuZC1zdG9jay1w/aG90by5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9TGpGVkRm/anVzV0JqWVRObGlI/VjlEeVhmQXBQR2M4/RG1nQkdFdGZWZ1Ew/UT0" alt="" />
-                <div className='bg-orange-200 h-full py-7'>
-                    <h1 className='py-3 px-14 font-bold  text-5xl '><p className='py-3 border-b border-orange-300'>About Us</p></h1>
-                    <p className='py-6 tracking-wide sm:px-14  font-semibold text-[16px] '>Welcome to our <strong>College Hub </strong>Web Application! We aim to provide an extensive and up-to-date database of <strong>IT and Management</strong> colleges across India. Our platform allows users to easily search and explore
+                <div className='bg-orange-200 h-full pb-6 lg:rounded-tr-2xl px-6 sm:px-0 pt-6 sm:pt-0'>
+                    <h1 className='py-3 sm:px-14 font-bold  text-4xl '><p className='py-3 border-b border-orange-300'>About Us</p></h1>
+                    <p className='py-6 text-2xl tracking-wide sm:px-14  font-semibold sm:text-[16px] leading-7 sm:leading-10'>Welcome to our <strong className='text-orange-700'>College Hub </strong>Web Application! We aim to provide an extensive and up-to-date database of <strong className='text-orange-700'>IT and Management</strong> colleges across India. Our platform allows users to easily search and explore
                         detailed information about various colleges, including courses offered, eligibility criteria, fees, and more .
                         
                         Whether you are a student, parent, or education enthusiast, our goal is to help you make informed decisions about higher education opportunities in the IT and Management fields.</p>
-                    <p className='text-right px-16 pb-2  font-semibold tracking-wide '><span className='border-b-2 border-orange-400 py-2'>Thank You !</span></p>
+                    <p className='text-right px-16 pb-2  font-semibold tracking-wide '><span className='border-b border-orange-400 py-2'>Thank You !</span></p>
                 </div>
 
             </div>
--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdMenu } from "react-icons/md";
-import { FaBookmark,FaGithub } from "react-icons/fa";
+import { FaBookmark,FaGithub,FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { useSidebarContext } from '../context/sidebar_context';
 import { useCartContext } from '../context/cart_context';
@@ -17,16 +17,17 @@ const Navbar = () => {
           <span className='py-3 rounded-xl px-2 bg-gradient-to-tr from-orange-500 to-gray-300'><span className="text-5xl  ">c</span >ollege </span><span className='text-orange-600 sm:text-5xl text-[28px] ml-1'>Hub</span>
         </Link>
         <SearchBar/>
-        <div className='flex gap-2 items-center pr-3'>
-          <a href="https://github.com/AYUSH-148/IT_AND_MANAG-Database/" target='_blank'><FaGithub className='text-4xl mr-3' /></a>
-          <Link to="/cart" className='relative mr-4 text-4xl'>
+        <div className='flex gap-2 items-center  pr-3'>
+          <a href="https://github.com/AYUSH-148/IT_AND_MANAG-Database/" target='_blank' className='order-3 md:order-1'><FaGithub className='text-4xl mr-3 ' /></a>
+          <Link to="/cart" className='relative mr-4 text-4xl order-2 md:order-2'>
             <FaBookmark />
             <span className='absolute right-0 top-0 transform translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white text-lg font-bold rounded-full w-6 h-6 flex items-center justify-center'>
               {total_items}
             </span>
           </Link>
-          <button type="button" className='text-4xl transition-opacity duration-300 hover:opacity-70' onClick={openSidebar}>
-            <MdMenu />
+          <button type="button" className='text-4xl order-1 md:order-3  transition-opacity duration-300 hover:opacity-70' onClick={openSidebar}>
+            <MdMenu className='md:block hidden'/>
+            <FaSearch className='md:hidden text-gray-600 mr-2'/>
           </button>
         </div>
       </div>
