2bdd60896fc6031eda86ada7ab08f7054f255f7c
Ayush Negi <ayushpro111@gmail.com>
2025-09-10 22:07:05 +0530

    Change DotSpinner size from h-3 w-3 to h-2 w-2

--- Frontend/src/components/DotSpinner.js
+++ Frontend/src/components/DotSpinner.js
@@ -3,10 +3,10 @@ import React from 'react';
 const DotSpinner = () => {
   return (
     <div className="flex justify-center items-center space-x-2">
-      <div className="dot-spinner animate-bounce bg-blue-500 h-3 w-3 rounded-full"></div>
-      <div className="dot-spinner animate-bounce bg-blue-500 h-3 w-3 rounded-full animation-delay-200"></div>
-      <div className="dot-spinner animate-bounce bg-blue-500 h-3 w-3 rounded-full animation-delay-400"></div>
-      <div className="dot-spinner animate-bounce bg-blue-500 h-3 w-3 rounded-full animation-delay-500"></div>
+      <div className="dot-spinner animate-bounce bg-blue-500 h-2 w-2 rounded-full"></div>
+      <div className="dot-spinner animate-bounce bg-blue-500 h-2 w-2 rounded-full animation-delay-200"></div>
+      <div className="dot-spinner animate-bounce bg-blue-500 h-2 w-2 rounded-full animation-delay-400"></div>
+      <div className="dot-spinner animate-bounce bg-blue-500 h-2 w-2 rounded-full animation-delay-500"></div>
     </div>
   );
 };
